f38e1d633f0b89c25871c89064a6f77567286e72
Berk Caputcu <berkcaputcu@gmail.com>
2013-07-09 22:51:44 -0400

    seeds changed to remote file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'CSV'
+require 'open-uri'
 
-CSV.foreach('stops.txt', :headers => true) do |csv_obj|
+CSV.foreach(open('http://dl.dropboxusercontent.com/u/3243565/stops.txt'), :headers => true) do |csv_obj|
 
 	Stop.create(code: csv_obj['stop_code'], name: csv_obj['stop_name'], lat: csv_obj['stop_lat'], long: csv_obj['stop_lon'])
 
