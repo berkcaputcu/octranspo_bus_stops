d64b12ff2899eb1a6b13c939ee330ce62e1c2a83
Berk Caputcu <berkcaputcu@gmail.com>
2013-07-19 20:38:48 -0400

    about

--- config/routes.rb
+++ config/routes.rb
@@ -65,6 +65,7 @@ OctranspoStopTimes::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'compare#select_stops'
+  match '/about' => 'pages#about'
 
   # See how all your routes lay out with "rake routes"
 
